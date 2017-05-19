72a2ca378134d035bfa7a7fa14fdabf6f25f150d
chamsmarena <chamsmarena@hotmail.com>
2017-05-19 13:02:13 +0000

    update project

--- Database/Requetes.sql
+++ Database/Requetes.sql
@@ -50,3 +50,93 @@ join categorie_actualite ca on ca.ID_CATEG_ACTU = a.ID_CATEG_ACTU
 join menaces m on m.ID_MENACE = a.ID_MENACE
  join admin_1 ad1 on ad1.ID_A1 = a.ID_A1
  join admin_0 ad0 on ad0.ID_A0 = ad1.ID_A0
+ 
+ 
+  /* v3 27/04/2017 */
+create view listeactualite as select 
+ca.TYPE_CATEG_ACTU AS TYPE_CATEG_ACTU,
+ca.TITRE_CATEG_ACTU AS TITRE_CATEG_ACTU,
+ad0.CODE_A0 AS CODE_A0,
+ad0.CAPTION_A0 AS CAPTION_A0,
+ad0.COORD_X_A0 AS COORD_X_A0,
+ad0.COORD_Y_A0 AS COORD_Y_A0,
+ad1.CODE_A1 AS CODE_A1,
+ad1.CAPTION_A1 AS CAPTION_A1,
+ad1.COORD_X_A1 AS COORD_X_A1,
+ad1.COORD_Y_A1 AS COORD_Y_A1,
+a.TITRE_ACTUALITE AS TITRE_ACTUALITE,
+a.DETAIL_ACTUALITE AS DETAIL_ACTUALITE,
+a.DATE_ACTUALITE AS DATE_ACTUALITE,
+a.ID_LANGUE,
+'0' AS VALEUR,
+ad0.ID_A0,
+ca.ID_CATEG_ACTU,
+m.CODE_MENACE,
+m.TITRE_MENACE,
+t.ID_TAG,
+t.CODE_TAG,
+t.LIBELLE_TAG,
+a.ID_ACTUALITE
+from actualite a 
+join categorie_actualite ca on ca.ID_CATEG_ACTU = a.ID_CATEG_ACTU
+join menaces m on m.ID_MENACE = a.ID_MENACE
+ join admin_1 ad1 on ad1.ID_A1 = a.ID_A1
+ join admin_0 ad0 on ad0.ID_A0 = ad1.ID_A0
+ join r_actualite_avoir_tag aat on aat.ID_ACTUALITE = a.ID_ACTUALITE
+ join tag t on t.ID_TAG = aat.ID_TAG
+ 
+ 
+ 
+   /* v4 05/05/2017 */
+create view listeactualite as select 
+ca.TYPE_CATEG_ACTU AS TYPE_CATEG_ACTU,
+ca.TITRE_CATEG_ACTU AS TITRE_CATEG_ACTU,
+ad0.CODE_A0 AS CODE_A0,
+ad0.CAPTION_A0 AS CAPTION_A0,
+ad0.COORD_X_A0 AS COORD_X_A0,
+ad0.COORD_Y_A0 AS COORD_Y_A0,
+ad1.CODE_A1 AS CODE_A1,
+ad1.CAPTION_A1 AS CAPTION_A1,
+ad1.COORD_X_A1 AS COORD_X_A1,
+ad1.COORD_Y_A1 AS COORD_Y_A1,
+a.TITRE_ACTUALITE AS TITRE_ACTUALITE,
+a.DETAIL_ACTUALITE AS DETAIL_ACTUALITE,
+a.DATE_ACTUALITE AS DATE_ACTUALITE,
+a.ID_LANGUE,
+'0' AS VALEUR,
+ad0.ID_A0,
+ca.ID_CATEG_ACTU,
+m.CODE_MENACE,
+m.TITRE_MENACE,
+t.ID_TAG,
+t.CODE_TAG,
+t.LIBELLE_TAG,
+a.ID_ACTUALITE,
+l.LIBELLE_LIEU,
+ad0Lieu.CAPTION_A0 AS CAPTION_A0Lieu,
+ad0Lieu.COORD_X_A0 AS COORD_X_A0Lieu,
+ad0Lieu.COORD_Y_A0 AS COORD_Y_A0Lieu,
+ad1Lieu.CODE_A1 AS CODE_A1Lieu,
+ad1Lieu.CAPTION_A1 AS CAPTION_A1Lieu,
+ad1Lieu.COORD_X_A1 AS COORD_X_A1Lieu,
+ad1Lieu.COORD_Y_A1 AS COORD_Y_A1Lieu
+from actualite a 
+join categorie_actualite ca on ca.ID_CATEG_ACTU = a.ID_CATEG_ACTU
+join menaces m on m.ID_MENACE = a.ID_MENACE
+join admin_1 ad1 on ad1.ID_A1 = a.ID_A1
+join admin_0 ad0 on ad0.ID_A0 = ad1.ID_A0
+join r_actualite_avoir_tag aat on aat.ID_ACTUALITE = a.ID_ACTUALITE
+join tag t on t.ID_TAG = aat.ID_TAG
+left join lieu l on l.ID_LIEU=a.ID_LIEU
+left join admin_1 ad1Lieu on ad1Lieu.ID_A1 = l.ID_A1
+left join admin_0 ad0Lieu on ad1Lieu.ID_A0 = ad0Lieu.ID_A0
+
+Create view Localisation as 
+SELECT l.ID_LIEU,l.LIBELLE_LIEU,a1.ID_A1,a1.CAPTION_A1,a0.ID_A0,a0.CAPTION_A0,a0.CODE_A0 FROM lieu l
+inner join admin_1 a1 on a1.ID_A1 = l.ID_A1
+inner join admin_0 a0 on a0.ID_A0 = a1.ID_A0
+
+create view Capitales as 
+SELECT a0.ID_A0,a0.CODE_A0,a1.ID_A1,a1.CAPTION_A1 FROM admin_0 a0
+inner join admin_1 a1 on a1.ID_A0 = a0.ID_A0
+where a1.EST_CAPITALE = true
--- Database/WHS database.sql
+++ Database/WHS database.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* Nom de SGBD :  MySQL 5.0                                     */
-/* Date de création :  27/04/2017 12:13:43                      */
+/* Date de création :  04/05/2017 16:11:22                      */
 /*==============================================================*/
 
 
@@ -13,6 +13,8 @@ drop table if exists ADMIN_1;
 drop table if exists CATEGORIE_ACTUALITE;
 
 drop table if exists LANGUE;
+
+drop table if exists LIEU;
 
 drop table if exists MENACES;
 
@@ -29,14 +31,15 @@ create table ACTUALITE
 (
    ID_ACTUALITE         char(22) not null,
    ID_LANGUE            char(22) not null,
+   ID_LIEU              char(22) not null,
    IDUSER               int not null,
    ID_A1                int not null,
    ID_MENACE            int not null,
    ID_CATEG_ACTU        int not null,
-   ID_ACTUALITE_GROUPE  varchar(22),
    TITRE_ACTUALITE      char(100),
    DETAIL_ACTUALITE     text,
    DATE_ACTUALITE       date,
+   LIEU_ACTUALITE       varchar(100),
    primary key (ID_ACTUALITE)
 );
 
@@ -62,8 +65,8 @@ create table ADMIN_1
    ID_A0                int not null,
    CODE_A1              char(30),
    CAPTION_A1           char(50),
-   COORD_X_A1           char(4),
-   COORD_Y_A1           char(4),
+   COORD_X_A1           char(8),
+   COORD_Y_A1           char(8),
    primary key (ID_A1)
 );
 
@@ -86,6 +89,19 @@ create table LANGUE
    ID_LANGUE            char(22) not null,
    TITRE_LANGUE         char(22),
    primary key (ID_LANGUE)
+);
+
+/*==============================================================*/
+/* Table : LIEU                                                 */
+/*==============================================================*/
+create table LIEU
+(
+   ID_LIEU              char(22) not null,
+   ID_A1                int not null,
+   CODE_LIEU            varchar(10),
+   LIBELLE_LIEU         char(100),
+   HAS_ADMIN1           bool,
+   primary key (ID_LIEU)
 );
 
 /*==============================================================*/
@@ -135,6 +151,9 @@ create table UTILISATEUR
 alter table ACTUALITE add constraint FK_R_ACTUALITE_AVOIR_LANGUE foreign key (ID_LANGUE)
       references LANGUE (ID_LANGUE) on delete restrict on update restrict;
 
+alter table ACTUALITE add constraint FK_R_ACTUALITE_AVOIR_LIEU foreign key (ID_LIEU)
+      references LIEU (ID_LIEU) on delete restrict on update restrict;
+
 alter table ACTUALITE add constraint FK_R_ACTUALITE_AVOIR_MENACES foreign key (ID_MENACE)
       references MENACES (ID_MENACE) on delete restrict on update restrict;
 
@@ -149,6 +168,9 @@ alter table ACTUALITE add constraint FK_R_USER_ADD_ACTUALITE foreign key (IDUSER
 
 alter table ADMIN_1 add constraint FK_R_ADMIN1_AVOIR_ADMIN0 foreign key (ID_A0)
       references ADMIN_0 (ID_A0) on delete restrict on update restrict;
+
+alter table LIEU add constraint FK_R_LIEU_AVOIR_ADMIN_1 foreign key (ID_A1)
+      references ADMIN_1 (ID_A1) on delete restrict on update restrict;
 
 alter table R_ACTUALITE_AVOIR_TAG add constraint FK_R_ACTUALITE_AVOIR_TAG foreign key (ID_ACTUALITE)
       references ACTUALITE (ID_ACTUALITE) on delete restrict on update restrict;
